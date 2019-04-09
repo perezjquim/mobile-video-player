998c67b22cb84f1287d298107322c032c7d9a97a
Joaquim Perez <jquim1@hotmail.com>
2019-04-09 19:01:56 +0100

    permissions check init

--- app/src/main/java/com/perezjquim/ssui/MainActivity.java
+++ app/src/main/java/com/perezjquim/ssui/MainActivity.java
@@ -1,20 +1,41 @@
 package com.perezjquim.ssui;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.os.*;
+import android.content.*;
 
-import com.perezjquim.UIHelper;
+import com.perezjquim.*;
 
 public class MainActivity extends AppCompatActivity
 {
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        /***********/
+        /*** init ***/
         super.onCreate(savedInstanceState);
         super.setTheme(R.style.AppTheme);
-
         setContentView(R.layout.activity_main);
+        PermissionChecker.init(this);
+        /*** init ***/
+        /***********/
 
         UIHelper.toast(this,"test");
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode)
+        {
+            /*** permissões ***/
+            /*********************/
+            case PermissionChecker.REQUEST_CODE:
+                PermissionChecker.restart();
+                break;
+            /********************/
+            /********************/
+        }
     }
 }
