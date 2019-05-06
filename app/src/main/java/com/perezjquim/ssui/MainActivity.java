7a3d160aab701352437da9b5c2156e751f22670c
LuisFreitas97 <luisfreitas803@gmail.com>
2019-05-06 20:35:19 +0100

    hghg

--- app/src/main/java/com/perezjquim/ssui/MainActivity.java
+++ app/src/main/java/com/perezjquim/ssui/MainActivity.java
@@ -1,13 +1,22 @@
 package com.perezjquim.ssui;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.*;
 import android.content.*;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.MediaController;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 import com.perezjquim.*;
 
 public class MainActivity extends AppCompatActivity
 {
+    private static final int REQUEST_TAKE_GALLERY_VIDEO = 2;
+    private Toolbar mTopToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -17,10 +26,42 @@ public class MainActivity extends AppCompatActivity
         super.setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
         PermissionChecker.init(this);
-        /*** init ***/
-        /***********/
+        mTopToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(mTopToolbar);
 
-        UIHelper.toast(this,"test");
+        openIntent();
+
+        //UIHelper.toast(this,"test");
+    }
+
+    public void openIntent(){
+        Intent intent = new Intent();
+        intent.setType("video/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent,"Select Video"),REQUEST_TAKE_GALLERY_VIDEO);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_folder) {
+            openIntent();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -34,8 +75,17 @@ public class MainActivity extends AppCompatActivity
             case PermissionChecker.REQUEST_CODE:
                 PermissionChecker.restart();
                 break;
-            /********************/
-            /********************/
+
+            case REQUEST_TAKE_GALLERY_VIDEO:
+                Uri selectedVideoUri = data.getData();
+                VideoView videoView =(VideoView)findViewById(R.id.vdVw);
+                MediaController mediaController= new MediaController(this);
+                mediaController.setAnchorView(videoView);
+                videoView.setMediaController(mediaController);
+                videoView.setVideoURI(selectedVideoUri);
+                videoView.requestFocus();
+                videoView.start();
+                break;
         }
     }
 }
